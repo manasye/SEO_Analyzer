00e0df51158f1a93734ae0aeef01ffd57966ecd0
Manasye Shousen Bukit <32813070+manasye@users.noreply.github.com>
2017-11-29 06:08:37 +0700

    remove duplicate

--- main.py
+++ main.py
@@ -10,15 +10,28 @@ def trade_spider(max):
         else :
             url = 'http://www.yify-torrent.org/popular-' + str(page) + '.html'
             print(url)
-        print('\nList of url in popular page ' + str(page) + '\n')
+        print('\nList of url in popular page ' + str(page) + ':\n')
+
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text,"html.parser")
+
+        listlink = []
+        purelink = [] # List that contain no duplicate
+
         for link in soup.findAll('a',{'target' : '_blank'}):
             href = link.get('href')
+            # listhref to ommit a non downloaded format
             listhref = list(href)
             if(listhref[0] == '/'):
-                print('http://www.yify-torrent.org' + href)
+                listlink.append(str(href))
+
+        for link in listlink:
+            if link not in purelink:
+                purelink.append(link) # Make unique list
+
+        for link in purelink:
+            print('http://www.yify-torrent.org' + link + '\n')
         page += 1
 
 trade_spider(5)
