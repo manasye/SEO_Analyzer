9b4a0dcea7bfcbf515f220be521fa0a1063acf50
Manasye Shousen Bukit <32813070+manasye@users.noreply.github.com>
2017-11-30 08:21:28 +0700

    gettin smarter

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ def trade_spider(max):
     while page <= max :
         if(page == 1) :
             url = 'http://www.yify-torrent.org/popular.html'
-            print(url)
         else :
             url = 'http://www.yify-torrent.org/popular-' + str(page) + '.html'
-            print(url)
-        print('\nList of url in popular page ' + str(page) + ':\n')
+        print('\nList of url in  ' + url + ' :\n')
 
         source_code = requests.get(url)
         plain_text = source_code.text
@@ -23,15 +21,15 @@ def trade_spider(max):
             href = link.get('href')
             # listhref to ommit a non downloaded format
             listhref = list(href)
-            if(listhref[0] == '/'):
-                listlink.append(str(href))
+            if(listhref[0] == '/') and (listhref[1] == 'm'):
+                listlink.append('http://www.yify-torrent.org' + str(href))
 
         for link in listlink:
             if link not in purelink:
                 purelink.append(link) # Make unique list
 
         for link in purelink:
-            print('http://www.yify-torrent.org' + link + '\n')
+            print(link + '\n')
         page += 1
 
 trade_spider(5)
