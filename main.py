5e758eab83c3f8c4dae1014c359809693b5d2ade
manasye <manase32@gmail.com>
2020-03-25 07:37:16 +0700

    Tidy up

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ HOMEPAGE = 'http://www.goal.com/id'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
-NUMBER_OF_THREADS = 1
+NUMBER_OF_THREADS = 4
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
